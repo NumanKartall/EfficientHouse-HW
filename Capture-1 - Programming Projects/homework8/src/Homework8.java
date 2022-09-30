8aa57f47baeed42efabb3642f067ddc7d8dd52fa
NumanKartall <93405692+NumanKartall@users.noreply.github.com>
2022-09-30 21:26:39 +0300

    Update Homework8.java

--- Capture-1 - Programming Projects/homework8/src/Homework8.java
+++ Capture-1 - Programming Projects/homework8/src/Homework8.java
@@ -8,7 +8,7 @@ public class Homework8 {
 
         System.out.println("This program calculates vehicle average speed given a time and distance traveled.");
 
-        System.out.printf("Car average speed is  %f  miles per hour.",avarageSpeed);
+        System.out.printf("Car average speed is  %.2f  miles per hour.",avarageSpeed);
 
         //This program calculates vehicle average speed given a time and distance traveled.
         //Car average speed is  8,780488  miles per hour.
